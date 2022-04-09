80caf4a1f84e48c56d0d98e019895ac9ac2f5df8
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2022-04-09 14:57:10 +0700

    chore: update footer

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <Calculation v-if="$store.state.clicked" />
     <Grafik v-if="$store.state.clicked" />
     <div class="footer">
-      Copyright &copy; 2017 - <a target="_blank" href="https://instagram.com/muhsatrio__">Muhammad Satrio Wicaksono</a><br>
+      Copyright &copy; 2017 - <a target="_blank" href="https://msatrio.com">Muhammad Satrio Wicaksono</a><br>
       Created to support <a target="_blank" href="https://labfisdas-telu.com">Laboratorium Fisika Dasar Telkom University</a><br>
       Source code could be seen <a href="https://github.com/muhsatrio/regresi-linear-v2" target="_blank">here</a><br>
       <b>#TRA</b> <b>#MSIAYE</b><br>
